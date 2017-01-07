698afaaf521ae3017d5719bc0bc944fa921f78c3
roonilwazlib7 <roonilwazlib7@hotmail.com>
2017-01-07 09:40:34 -0700

    site is looking god

--- scripts/main.js
+++ scripts/main.js
@@ -4,27 +4,33 @@ $(document).ready( function(){
     var games = [
         {
             name: "OpenSpace",
-            version: "alpha1"
+            version: "alpha1",
+            devStatus: "alpha"
         },
         {
             name: "Knackered",
-            version: "alpha1"
+            version: "alpha1",
+            devStatus: "closed"
         },
         {
             name: "Bounce",
-            version: "alpha1"
+            version: "alpha1",
+            devStatus: "released"
         },
         {
             name: "Fantactics",
-            version: "alpha1"
+            version: "alpha1",
+            devStatus: "closed"
         },
         {
             name: "Smiley",
-            version: "alpha1"
+            version: "alpha1",
+            devStatus: "released"
         },
         {
             name: "Asteroids",
-            version: "alpha1"
+            version: "alpha1",
+            devStatus: "released"
         }
     ];
 
@@ -34,7 +40,44 @@ $(document).ready( function(){
     for (var i = 0; i < games.length; i++)
     {
         var game = games[i];
-        html = template.clone();
+        var html = template.clone();
+        var devStatus = html.find(".dev-status");
+
+        switch (game.devStatus)
+        {
+            case "alpha":
+                devStatus.css({
+                    "border-color": "orange",
+                    "color": "orange"
+                });
+                devStatus.html("Alpha");
+                break;
+
+            case "beta":
+                devStatus.css({
+                    "border-color": "blue",
+                    "color": "blue"
+                });
+                devStatus.html("Beta");
+                break;
+
+            case "released":
+                devStatus.css({
+                    "border-color": "green",
+                    "color": "green"
+                });
+                devStatus.html("Released");
+                break;
+
+            case "closed":
+                devStatus.css({
+                    "border-color": "red",
+                    "color": "red"
+                });
+                devStatus.html("Closed");
+                break;
+
+        }
 
         html.find("h2").html( game.name );
         html.find("img").attr("src", "images/" + game.name + ".png");
